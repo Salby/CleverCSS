cfc77ba44f7dca5b3f05a2af8aeb99697c702782
Sherlock <SanderSalamander@gmail.com>
2017-04-10 23:15:00 +0200

    Updated clever.css and clever.js

--- clever.js
+++ clever.js
@@ -1,21 +1,16 @@
-//DRAWER ANIMATION
-  //DRAWER
-  var drawer = document.getElementById('mobileDrawer')
-  //DRAWER OPEN BUTTON
-  var buttonOpen = document.getElementById('drawerButton')
-  //DRAWER CLOSE BUTTON
-  var buttonClose = document.getElementById('drawerClose')
-//OPEN DRAWER WHEN USER CLICKS ON BUTTON
-buttonOpen.onclick = function() {
-  drawer.className = "drawer animateOut";
-}
-//CLOSE DRAWER WHEN USER CLICKS THE CLOSE BUTTON
-buttonClose.onclick = function() {
-  drawer.className = "drawer animateIn";
-}
-//CLOSE DRAWER WHEN USER CLICKS ANYWHERE OUTSIDE THE DRAWER
-window.onclick = function(event) {
-  if (event.target == drawer) {
-      drawer.className = "drawer animateIn";
-  }
-}
+// DRAWER ANIMATION
+$(document).ready(function() {
+  $("#drawerButton").click(function() {
+    $("#mobileDrawer").addClass("drawerAnimate");
+  });
+  $("#drawerClose").click(function() {
+    $("#mobileDrawer").removeClass("drawerAnimate");
+  });
+  $(".buttonDropdownSmall").click(function() {
+    if ($(this).next().hasClass("dropdownContentShow")) {
+        $(this).next().removeClass("dropdownContentShow");
+      } else {
+        $(this).next().addClass("dropdownContentShow");
+      }
+  });
+});
