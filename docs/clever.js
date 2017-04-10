f819806890926c9ef1923409a2e09dac8cc68678
Sherlock <SanderSalamander@gmail.com>
2017-04-10 17:50:49 +0200

    Fixed a lot of things

--- docs/clever.js
+++ docs/clever.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
   $("#drawerClose").click(function() {
     $("#mobileDrawer").removeClass("drawerAnimate");
   });
+  $(".buttonDropdownSmall").click(function() {
+    if ($(this).next().hasClass("dropdownContentShow")) {
+        $(this).next().removeClass("dropdownContentShow");
+      } else {
+        $(this).next().addClass("dropdownContentShow");
+      }
+  });
 });
