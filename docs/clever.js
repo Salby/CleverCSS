cce602b1c339b1994e94b089a09aeeb6123f670c
Sherlock <SanderSalamander@gmail.com>
2017-04-10 16:42:36 +0200

    Started work on version 1.0 - a complete structural revision and remake of all the components in the framework

--- docs/clever.js
+++ docs/clever.js
@@ -1,21 +1,9 @@
-//DRAWER ANIMATION
-  //DRAWER
-  var drawer = document.getElementById('mobileDrawer')
-  //DRAWER OPEN BUTTON
-  var buttonOpen = document.getElementById('drawerButton')
-  //DRAWER CLOSE BUTTON
-  var buttonClose = document.getElementById('drawerClose')
-//OPEN DRAWER WHEN USER CLICKS ON BUTTON
-buttonOpen.onclick = function() {
-  drawer.className = "drawer animateOut";
-}
-//CLOSE DRAWER WHEN USER CLICKS THE CLOSE BUTTON
-buttonClose.onclick = function() {
-  drawer.className = "drawer animateIn";
-}
-//CLOSE DRAWER WHEN USER CLICKS ANYWHERE OUTSIDE THE DRAWER
-window.onclick = function(event) {
-  if (event.target == drawer) {
-      drawer.className = "drawer animateIn";
-  }
-}
+// DRAWER ANIMATION
+$(document).ready(function() {
+  $("#drawerButton").click(function() {
+    $("#mobileDrawer").addClass("drawerAnimate");
+  });
+  $("#drawerClose").click(function() {
+    $("#mobileDrawer").removeClass("drawerAnimate");
+  });
+});
