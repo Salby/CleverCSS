107917b93987e4c77b9e604f6b6b0b75616134a2
Sherlock <SanderSalamander@gmail.com>
2017-04-11 19:01:44 +0200

    Updated clever.css and clever.js

--- clever.js
+++ clever.js
@@ -1,16 +1,17 @@
-// DRAWER ANIMATION
 $(document).ready(function() {
+  // DRAWER ANIMATION
   $("#drawerButton").click(function() {
     $("#mobileDrawer").addClass("drawerAnimate");
   });
   $("#drawerClose").click(function() {
     $("#mobileDrawer").removeClass("drawerAnimate");
   });
+  // DROPDOWN ANIMATION
   $(".buttonDropdownSmall").click(function() {
     if ($(this).next().hasClass("dropdownContentShow")) {
         $(this).next().removeClass("dropdownContentShow");
-      } else {
+    } else {
         $(this).next().addClass("dropdownContentShow");
-      }
+    }
   });
 });
