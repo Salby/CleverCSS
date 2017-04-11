6e962fb3516f1ad6789f5f238b1a4ff5478a3874
Sherlock <SanderSalamander@gmail.com>
2017-04-11 19:00:17 +0200

    Fixed drawer issues | Fixed button padding | Added comments to clever.js

--- docs/clever.js
+++ docs/clever.js
@@ -1,11 +1,12 @@
-// DRAWER ANIMATION
 $(document).ready(function() {
+  // DRAWER ANIMATION
   $("#drawerButton").click(function() {
     $("#mobileDrawer").addClass("drawerAnimate");
   });
   $("#drawerClose").click(function() {
     $("#mobileDrawer").removeClass("drawerAnimate");
   });
+  // DROPDOWN ANIMATION
   $(".buttonDropdownSmall").click(function() {
     if ($(this).next().hasClass("dropdownContentShow")) {
         $(this).next().removeClass("dropdownContentShow");
