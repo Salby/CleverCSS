96521a5df3ae44bd8b550bf731138660395c4ade
Sherlock <SanderSalamander@gmail.com>
2017-05-13 15:31:13 +0200

    Added fabMenu

--- docs/clever.js
+++ docs/clever.js
@@ -31,4 +31,14 @@ $(document).ready(function() {
     var modalTargetClose = $(this).data("target");
     $(modalTargetClose).removeClass("modalActive");
   });
+  // FLOATING ACTION BUTTON MENU
+  $(".fabMenuContainer .fabMenuButton").click(function() {
+    if ($(".fabMenuContainer .fabMenuButton").hasClass("fabMenuButtonActive")) {
+        $(".fabMenuContainer .fabMenuButton").removeClass("fabMenuButtonActive");
+        $(".fabMenuContainer .fabMenuList").removeClass("fabMenuListActive").addClass("fabMenuListInactive");
+    } else {
+      $(".fabMenuContainer .fabMenuButton").addClass("fabMenuButtonActive");
+      $(".fabMenuContainer .fabMenuList").removeClass("fabMenuListInactive").addClass("fabMenuListActive");
+    }
+  })
 });
