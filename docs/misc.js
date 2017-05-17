049bc1ac27c52c6d7caafcacc032656e347730fb
Sherlock <SanderSalamander@gmail.com>
2017-05-17 14:17:55 +0200

    Added indicator to index

--- docs/misc.js
+++ docs/misc.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // SMOOTH SCROLLING
-  $("a[data-scroll-target]").click(function() {
+  $("*[data-scroll-target]").click(function() {
     var scrollTarget = $(this).data("scroll-target");
     $("html, body").animate({
       scrollTop: $(scrollTarget).offset().top
@@ -13,22 +13,19 @@ $(document).ready(function() {
     $(target).removeClass("hideComp").addClass("showComp");
   });
   // SCROLL STUFF
-  var menu = $(".elementList");
-  var heightThreshold = $(".elementDisplay").offset().top;
-  var heightThreshold_end = $(".elementDisplay").offset().top +$(".elementDisplay").height();
-  var components = $("#componentsWrapper");
-  var scrollTrigger_end = $("#getStarted").offset().top +$("#getStarted").height();
   $(window).scroll(function() {
+    var menu = $(".elementList");
+    var heightThreshold = $(".elementDisplay").offset().top;
+    var heightThreshold_end = $(".elementDisplay").offset().top +$(".elementDisplay").height();
+    var components = $("#componentsWrapper");
+    var scrollTrigger_end = $("#getStarted").offset().top +$("#getStarted").height();
+    var scrollTriggerNav = $("#title").offset().top;
     var scroll = $(window).scrollTop();
 
-    if (scroll >= heightThreshold - 66 && scroll < heightThreshold_end - 66) {
+    if (scroll >= heightThreshold - 66) {
         menu.addClass("elementListPin");
     } else {
         menu.removeClass("elementListPin");
-    }
-
-    if (scroll < scrollTrigger_end) {
-        components.addClass("animateWrapper")
     }
   });
 });
