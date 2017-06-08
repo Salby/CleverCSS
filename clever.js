48fa01124eafab8bf1a53c9f2be4468737aa5fa3
Sherlock <SanderSalamander@gmail.com>
2017-06-08 11:37:16 +0200

    Updated clever.css and clever.js

--- clever.js
+++ clever.js
@@ -1,55 +1,46 @@
 $(document).ready(function() {
   // DRAWER ANIMATION
-  $("#drawerOpen").click(function(e) {
+  $('#drawer-open').click(function(e) {
     e.stopPropagation()
-    $("#mobileDrawer").removeClass("drawerAnimateClose").addClass("drawerAnimateOpen");
+    $('#mobile-drawer').removeClass('drawerAnimateClose').addClass('drawerAnimateOpen');
   });
-  $("#drawerClose").click(function() {
-    $("#mobileDrawer").removeClass("drawerAnimateOpen").addClass("drawerAnimateClose");
+  $('#drawer-close').click(function() {
+    $('#mobile-drawer').removeClass('drawerAnimateOpen').addClass('drawerAnimateClose');
   });
   // DROPDOWN ANIMATION
-  $(".buttonDropdownSmall").click(function() {
-    if ($(this).next().hasClass("dropdownSmallContentShow")) {
-        $(this).next().removeClass("dropdownSmallContentShow");
-    } else {
-        $(this).next().addClass("dropdownSmallContentShow");
-    }
-  });
-  $(".buttonDropdownLarge").click(function() {
-    if ($(this).next().hasClass("dropdownLargeContentShow")) {
-        $(this).next().removeClass("dropdownLargeContentShow").addClass("dropdownLargeContentHide");
-    } else {
-        $(this).next().removeClass("dropdownLargeContentHide").addClass("dropdownLargeContentShow");
-    }
+  $('.button-dropdown').click(function() {
+    var dropdownWidth = $(this).next().width();
+    var dropdownHeight = $(this).next().height();
+    $(this).next().slideToggle('fast');
   });
   // MODAL THINGS
-  $(".buttonModalOpen[data-target]").click(function() {
-    //alert($(this).data("target"));
-    var modalTargetOpen = $(this).data("target");
-    $(modalTargetOpen).css("display", "flex");
-    if ($(modalTargetOpen).hasClass("modalInactive")) {
-        $(modalTargetOpen).removeClass("modalInactive").addClass("modalActive");
+  $('.buttonModalOpen[data-target]').click(function() {
+    //alert($(this).data('target'));
+    var modalTargetOpen = $(this).data('target');
+    $(modalTargetOpen).css('display', 'flex');
+    if ($(modalTargetOpen).hasClass('modalInactive')) {
+        $(modalTargetOpen).removeClass('modalInactive').addClass('modalActive');
     } else {
-        $(modalTargetOpen).addClass("modalActive")
+        $(modalTargetOpen).addClass('modalActive')
     }
-    $("body").css("overflow-y", "hidden");
+    $('body').css('overflow-y', 'hidden');
   });
-  $(".buttonModalClose[data-target]").click(function() {
-    var modalTargetClose = $(this).data("target");
-    $(modalTargetClose).removeClass("modalActive").addClass("modalInactive");
+  $('.buttonModalClose[data-target]').click(function() {
+    var modalTargetClose = $(this).data('target');
+    $(modalTargetClose).removeClass('modalActive').addClass('modalInactive');
     setTimeout(function() {
-      $(modalTargetClose).css("display", "none");
+      $(modalTargetClose).css('display', 'none');
     }, 300);
-    $("body").css("overflow-y", "visible");
+    $('body').css('overflow-y', 'visible');
   });
   // FLOATING ACTION BUTTON MENU
-  $(".fabMenuContainer .fabMenuButton").click(function() {
-    if ($(this).hasClass("fabMenuButtonActive")) {
-        $(this).removeClass("fabMenuButtonActive");
-        $(".fabMenuContainer .fabMenuList").removeClass("fabMenuListActive").addClass("fabMenuListInactive");
+  $('.fabMenuContainer .fabMenuButton').click(function() {
+    if ($(this).hasClass('fabMenuButtonActive')) {
+        $(this).removeClass('fabMenuButtonActive');
+        $('.fabMenuContainer .fabMenuList').removeClass('fabMenuListActive').addClass('fabMenuListInactive');
     } else {
-        $(this).addClass("fabMenuButtonActive");
-        $(".fabMenuContainer .fabMenuList").removeClass("fabMenuListInactive").addClass("fabMenuListActive");
+        $(this).addClass('fabMenuButtonActive');
+        $('.fabMenuContainer .fabMenuList').removeClass('fabMenuListInactive').addClass('fabMenuListActive');
     }
   });
 });
