808728e0455ab6e939dcfe79ef34cc12c98ccd17
Sherlock <SanderSalamander@gmail.com>
2017-06-18 15:02:55 +0200

    Fixed anchors in navbars

--- docs/scroll.js
+++ docs/scroll.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
   var scroll = $(window).scrollTop();
   var navShadowTrigger = $('#getStarted').offset().top;
 
-  if (scroll >= navShadowTrigger - 56) {
+  if (scroll >= navShadowTrigger - 60) {
     $('.navbar').removeClass('navbar-no-shadow');
   } else {
     $('.navbar').addClass('navbar-no-shadow');
